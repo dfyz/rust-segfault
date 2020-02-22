19bd9e018a599758fa6975b81b04473624dc1f3e
Ivan Komarov <Ivan.Komarov@dfyz.info>
2020-02-22 23:42:23 +0300

    Get rid of the array

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 fn do_test(x: usize) {
-    let mut arr = [[0]];
     let mut z = Vec::new();
-    for arr_ref in &mut arr {
+    for _ in 0..1 {
         for y in 0..x {
             for _ in 0..1 {
                 z.extend(std::iter::repeat(0).take(x));
                 let a = y * x;
                 let b = (y + 1) * x - 1;
-                let slice = &mut arr_ref[a..b];
+                let slice = &mut z[a..b];
                 slice[1 << 24] += 1;
             }
         }
